4a57c02490843ea190f57724d3bc47b44b32dd1c
SmolSoft <smolsoft@email.cz>
2023-10-22 21:25:42 +0200

    NoteController.java added CRUD methods

--- src/main/java/com/smolgeek/noteapp/controllers/NoteController.java
+++ src/main/java/com/smolgeek/noteapp/controllers/NoteController.java
@@ -1,35 +1,101 @@
 package com.smolgeek.noteapp.controllers;
 
+import com.smolgeek.noteapp.models.dto.NoteDTO;
+import com.smolgeek.noteapp.models.services.NotesService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
 
-@RestController
+/**
+ * All notes are accessible under the @RequestMapping("/notes")
+ */
+
+@Controller
 @RequestMapping("/notes")
 public class NoteController {
 
-
-    //todo
-    //  create
-    //  create form
-    //  delete
-    //  edit
-    //  get all
-    //  exception
+    @Autowired
+    NotesService notesService;
 
 
+    //--- Get notes ---
+
+    /** Get all notes */
+    @GetMapping
+    public String renderNotes(Model model) {
+        List<NoteDTO> notes = notesService.getAll();
+        model.addAttribute("notes", notes);
+        return "pages/notes/index";
+    }
+
+    /**
+     * @Returns note's full details
+     */
+    @GetMapping("/{noteId}")
+    public String renderDetail(@PathVariable long noteId, Model model) {
+
+        //Find note by id
+        NoteDTO note = notesService.getById(noteId);
+        model.addAttribute("note", note);
+        return "pages/articles/detail";
+    }
+
+
+    //--- create note ---
+
+    /** Create form at "pages/notes/create" */
+    @GetMapping("create")
+    private String renderCreateForm(@ModelAttribute NoteDTO note) {
+        return "pages/notes/create";
+    }
+
+    /**
+     * Saves newly created note. <p>
+     * Implements `@Valid` jakarta validation to validate inserted data, e.g. number of characters in title field.
+     */
     @PostMapping("create")
-    private String createNote(RedirectAttributes redirectAttributes) {
-        //todo validation
-        redirectAttributes.addFlashAttribute("success", "New note created"); //message back to the user
+    private String createNote(@Valid @ModelAttribute NoteDTO note, BindingResult result, RedirectAttributes redirectAttributes) {
+
+        //validation
+        if (result.hasErrors()) return renderCreateForm(note);
+
+        //adds new note into data store
+        notesService.create(note);
+
+        //message for the user
+        redirectAttributes.addFlashAttribute("success", "New note was created");
+
         return "redirect:/notes";
     }
 
-    @GetMapping("/{noteId}")
-    public String renderDetail(@Valid @PathVariable long noteId, Model model) {
-        return null;
+
+    //--- edit note ---
+
+    @GetMapping("/edit/{noteId}")
+    private String renderEditFrom(@PathVariable Long noteId, NoteDTO note) {
+        NoteDTO noteDTO = notesService.getById(noteId);
+        notesService.updateNoteDTO(noteDTO, note);
+        return "pages/notes/edit";
     }
+
+
+    @PostMapping("/edit/{noteId}")
+    public String editNote(@PathVariable Long noteId, @Valid NoteDTO note, BindingResult result, RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) return renderEditFrom(noteId, note);
+        note.setNoteId(noteId);
+        notesService.edit(note);
+
+        redirectAttributes.addFlashAttribute("success", "Note was edited.");
+        return "redirect:/notes";
+    }
+
+    //--- todo delete note ---
+
 
 }
