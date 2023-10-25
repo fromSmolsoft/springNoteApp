0c2455e705026b3e7719fd66437c26b6e39bc585
SmolSoft <smolsoft@email.cz>
2023-10-25 10:17:21 +0200

    SmokeTest-verifies whether content loads for tests

---
+++ src/test/java/com/smolgeek/noteapp/SmokeTest.java
@@ -0,0 +1,24 @@
+package com.smolgeek.noteapp.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class SmokeTest {
+
+    @Autowired
+    private NoteController controller;
+
+    /** Verifies that contexts loads and controller is created by it */
+    @Test
+    void contextLoads() {
+        assertThat(controller).isNotNull();
+    }
+
+
+
+
+}
