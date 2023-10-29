bb115cf3ae12dccaac5cc31833dc0498bdf38660
SmolSoft <smolsoft@email.cz>
2023-10-29 20:14:47 +0100

    SmokeTest.java

--- src/test/java/com/smolgeek/noteapp/SmokeTest.java
+++ src/test/java/com/smolgeek/noteapp/SmokeTest.java
@@ -1,5 +1,6 @@
-package com.smolgeek.noteapp.controllers;
+package com.smolgeek.noteapp;
 
+import com.smolgeek.noteapp.controllers.NoteController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,13 +13,11 @@ class SmokeTest {
     @Autowired
     private NoteController controller;
 
-    /** Verifies that contexts loads and controller is created by it */
+    /** Verifies whether context loads by creating controller. */
     @Test
     void contextLoads() {
         assertThat(controller).isNotNull();
     }
-
-
 
 
 }
