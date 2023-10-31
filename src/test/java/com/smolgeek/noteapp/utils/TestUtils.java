f78ee5fd9c27eba6b84bd78d7edcf3ff9674eaad
SmolSoft <smolsoft@email.cz>
2023-10-31 16:16:09 +0100

    TestUtils.java improvement

--- src/test/java/com/smolgeek/noteapp/utils/TestUtils.java
+++ src/test/java/com/smolgeek/noteapp/utils/TestUtils.java
@@ -10,7 +10,11 @@ import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+/** Testing helper class.  Mostly for generating dummy objects in consistent manner. */
 public class TestUtils {
+    public static final String NOTE_TITLE_DUMMY = " Title of Lorem Ipsum";
+    public static final String LOREM_IPSUM = " Content: Lorem ipsum dolor sit amet. Aut minima impedit ut sunt sint vel maiores tempore et odit sapiente et sint vero est dolorem quia.";
+    public static final String DATE = "2024-01-01T01:01:01.111+01:00";
 
     /**
      * Maps object to Json String.
@@ -21,69 +25,98 @@ public class TestUtils {
         return mapper.writeValueAsString(object);
     }
 
+
+    //Single objects generations
+
+    public static NoteDTO generateNoteDTO(Long id) {
+        String title = id + NOTE_TITLE_DUMMY;
+        String content = id + LOREM_IPSUM;
+        OffsetDateTime time = OffsetDateTime.parse(DATE);
+
+        return generateNoteDTO(id, title, content, time, time);
+    }
+
+    public static NoteDTO generateNoteDTO(Long id, String title, String content, OffsetDateTime created, OffsetDateTime modified) {
+        NoteDTO noteDTO = new NoteDTO();
+
+        if (id != null) noteDTO.setNoteId(id);
+
+        noteDTO.setTitle(title);
+        noteDTO.setContent(content);
+        noteDTO.setCreationDateTime(created);
+        noteDTO.setModifiedDateTime(modified);
+        return noteDTO;
+    }
+
+    public static NoteEntity generateEntity(Long id) {
+        String title = id + NOTE_TITLE_DUMMY;
+        String content = id + LOREM_IPSUM;
+        OffsetDateTime time = OffsetDateTime.parse(DATE);
+
+        return generateEntity(id, title, content, time, time);
+    }
+
+    public static NoteEntity generateEntity(Long id, String title, String content, OffsetDateTime created, OffsetDateTime modified) {
+        NoteEntity noteEntity = new NoteEntity();
+
+        if (id != null) noteEntity.setNoteId(id);
+
+        noteEntity.setTitle(title);
+        noteEntity.setContent(content);
+        noteEntity.setCreationDateTime(created);
+        noteEntity.setModifiedDateTime(modified);
+        return noteEntity;
+    }
+
+
+    //List generations
+
     /**
-     * Generates a list of NoteDTOs with the given number of elements.
-     * @param n the number of NoteDTOs to generate
-     * @return the list of generated NoteDTOs
+     * Generates a list of entities without ids.
+     * @param n the number generated elements
+     * @return the list of generated elements
      */
-    public static List<NoteEntity> generateNoteEntities(int n) {
-        OffsetDateTime time = OffsetDateTime.parse("2024-01-01T01:01:01.111+01:00");
+    public static List<NoteEntity> generateNoteEntitiesNoId(int n) {
+        OffsetDateTime time = OffsetDateTime.parse(DATE);
         List<NoteEntity> list = new ArrayList<>(n);
 
         for (int i = 0; i < n; i++) {
-            NoteEntity noteDTO = new NoteEntity();
-            noteDTO.setTitle(i + "# Title text");
-            noteDTO.setContent(i + "# Content text");
-            noteDTO.setCreationDateTime(time);
-            noteDTO.setModifiedDateTime(time);
-            list.add(noteDTO);
+            NoteEntity noteEntity = generateEntity(null);
+            list.add(noteEntity);
+        }
+        return list;
+    }
+
+    /**
+     * Generates a list of entities with ids.
+     * @param n the number generated elements
+     * @return the list of generated elements
+     */
+    public static List<NoteEntity> generateNoteEntitiesId(int n) {
+        List<NoteEntity> list = new ArrayList<>(n);
+
+        for (int i = 0; i < n; i++) {
+            NoteEntity noteEntity = generateEntity((long) i);
+            list.add(noteEntity);
         }
         return list;
     }
 
 
     /**
-     * Generates a list of NoteDTOs with the given number of elements.
+     * Generates a list of NoteDTOs with ids.
      * @param n the number of NoteDTOs to generate
      * @return the list of generated NoteDTOs
      */
-    public static List<NoteDTO> generateNoteDTOs(int n) {
-        OffsetDateTime time = OffsetDateTime.parse("2024-01-01T01:01:01.111+01:00");
+    public static List<NoteDTO> generateNoteDTOsId(int n) {
         List<NoteDTO> list = new ArrayList<>(n);
 
         for (int i = 0; i < n; i++) {
-            NoteDTO noteDTO = new NoteDTO();
-            noteDTO.setNoteId(i);
-            noteDTO.setTitle(i + "# Title text");
-            noteDTO.setContent(i + "# Content text");
-            noteDTO.setCreationDateTime(time);
-            noteDTO.setModifiedDateTime(time);
+            NoteDTO noteDTO = generateNoteDTO((long) i);
             list.add(noteDTO);
         }
         return list;
     }
 
 
-    /**
-     * Generates a list of JSON strings representing notes.
-     * @param n the number of notes to generate
-     * @return a list of JSON strings representing notes
-     */
-    private List<String> generateNoteJSons(int n) {
-        OffsetDateTime time = OffsetDateTime.parse("2024-01-01T01:01:01.111+01:00");
-        List<String> list = new ArrayList<>(n);
-
-        for (int i = 0; i < n; i++) {
-            String json =
-                    "{\n" +
-                    "    \"noteId\" : " + i + ",\n" +
-                    "    \"title\" : \"" + i + "# Title text\",\n" +
-                    "    \"content\" : \"" + i + "# Content text\",\n" +
-                    "    \"creationDateTime\" : \"" + time + "\",\n" +
-                    "    \"modifiedDateTime\" : \"" + time + "\"" +
-                    "\n}";
-            list.add(json);
-        }
-        return list;
-    }
 }
